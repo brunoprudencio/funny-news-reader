d1ad07f84de51964c671aa54af672d5176fd2ddb
Bruno Prudencio <brubatera5@gmail.com>
2020-12-08 22:36:36 -0300

    first commit

---
+++ src/main/java/com/juvenalicos/funnynewsreader/FunnyNewsReaderApplication.java
@@ -0,0 +1,13 @@
+package com.juvenalicos.funnynewsreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunnyNewsReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunnyNewsReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juvenalicos/funnynewsreader/FunnyNewsReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juvenalicos.funnynewsreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunnyNewsReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
