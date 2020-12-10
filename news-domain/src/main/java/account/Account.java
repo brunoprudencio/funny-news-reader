d6d8a96a0983cf6b7965078fe8923cdaeeaa7d72
Bruno Prudencio <brubatera5@gmail.com>
2020-12-09 22:49:25 -0300

    adding user

---
+++ news-domain/src/main/java/account/Account.java
@@ -0,0 +1,10 @@
+package account;
+
+import java.util.UUID;
+
+public class Account {
+
+	private UUID id;
+	private boolean active;
+	private AccountStatus status;
+}
---
+++ news-domain/src/main/java/account/AccountStatus.java
@@ -0,0 +1,6 @@
+package account;
+
+public enum AccountStatus {
+
+	PENDING, PROCESSING, CREATED, VERIFIED, WITH_ERROR, CLOSED
+}
